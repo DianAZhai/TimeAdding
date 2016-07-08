3a261228d5d9e7527626baa0bf662ca3c2cb46c2
DianAZhai <1031206016@qq.com>
2016-07-08 12:13:22 +0800

    The first change
    换了一个格式，界面看起来更加明朗

--- First.java
+++ First.java
@@ -1,3 +1,5 @@
+package Shujia;
+
 import java.util.Scanner;
 
 public class Minxiangjia {
@@ -5,11 +7,15 @@ public static void main(String[]args){
 	int m1,m2,m3,m4;
 	System.out.println("请输入两相加时间数：");
 	Scanner sc=new Scanner(System.in);
-
+	System.out.println("请输入分钟数");
 	m1=sc.nextInt();
-	m2=sc.nextInt();
+	System.out.println("请输入秒数");
 	m3=sc.nextInt();
+	System.out.println("请输入分钟数");
+	m2=sc.nextInt();
+	System.out.println("请输入秒数");
 	m4=sc.nextInt();
+	
 	int min=m1+m2;
 	int sec=m3+m4;
 	int Chushu;
@@ -21,7 +27,8 @@ public static void main(String[]args){
 	}else{
 		System.out.print(min+(sec/60)+"分钟"+sec%60+"秒");
 	}
-
+	
+	
 }
-}//这只是第一步，计算得出两个时间段相加，但要是想大量实现功能，还是得活用控制台
+}
 
