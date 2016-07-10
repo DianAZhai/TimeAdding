b2836ccfe7b9d93391417bccd9b6d6521f05fea8
DianAZhai <1031206016@qq.com>
2016-07-10 15:48:52 +0800

    some little change
    在输出n的时候不会再出现输入时间的情况

--- First.java
+++ First.java
@@ -1,5 +1,4 @@
-package Shujia;
-
+package Thread;
 import java.util.Scanner;
 
 public class Minxiangjia {
@@ -29,7 +28,7 @@ public static void main(String[]args){
  		System.out.println(min+(sec/60)+"分钟"+sec%60+"秒");
  	}
 
- 	  boolean isContinued = false;;
+ 	  boolean isContinued = false;
       do {
           isContinued = false;
           Scanner in = new Scanner(System.in);
@@ -41,31 +40,31 @@ public static void main(String[]args){
           char input = in.next().charAt(0);
           if (input == 'y') {
               isContinued = true;
+              System.out.println("请输入分钟数");
+              
+              int m  = in.nextInt();
+              System.out.println("请输入秒数");
+              int n = in.nextInt();//输出的就是秒数,这里的问题就是输入的数只能加上min或者是sec
+              int min1=min+m;
+              int sec1=sec+n;              
+              int Chushu1=min1/60;
+           	System.out.print(Chushu1+"小时");
+           	
+           	if(min1+(sec1/60)/60>=1){
+           		System.out.println((min1+(sec1/60))%60+"分钟"+sec1%60+"秒");
+           	}else{
+           		System.out.println(min1+(sec1/60)+"分钟"+sec1%60+"秒");
+           	}
+
           } else if (input == 'n') {
               isContinued = false;
           } else {
               System.out.println("您的输入无法被识别，默认结束！");
           }
-          System.out.println("请输入分钟数");
-      
-          int m  = in.nextInt();
-          System.out.println("请输入秒数");
-          int n = in.nextInt();//输出的就是秒数,这里的问题就是输入的数只能加上min或者是sec
-          int min1=min+m;
-          int sec1=sec+n;              
-          int Chushu1=min1/60;
-       	System.out.print(Chushu1+"小时");
-       	
-       	if(min1+(sec1/60)/60>=1){
-       		System.out.println((min1+(sec1/60))%60+"分钟"+sec1%60+"秒");
-       	}else{
-       		System.out.println(min1+(sec1/60)+"分钟"+sec1%60+"秒");
-       	}
-
+          
       } 
           while (isContinued);
       System.out.println("运行结束");
    
 }
 }//这里的问题就是在while循环里面不能修改min1和sec1的值，导致输入的数到最后都会加上原来的那组相加的数据
-
